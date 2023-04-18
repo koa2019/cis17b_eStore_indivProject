15a4f1fcfb373a2f3261bfaf291837c527011058
koa2019 <no-reply>
2023-04-18 02:44:09 -0700

    v1.2 and added yahtzee_v28.2, so I can merge it with v1.2.
    Signed-off-by: koa2019 <no-reply>

---
+++ store_code_cpp/template_v1.2_adminUser_classAggregate/NewClass.cpp
@@ -0,0 +1,70 @@
+#include <iostream>  // cout
+#include <iomanip>  // setw()
+#include <vector>  // vector
+#include <bits/stdc++.h>  // vectors pc
+#include "NewClass.h"
+#include "User.h"       // aggregates instance of User class
+
+using namespace std;
+
+//**********************************************************
+//                  Default Constructor
+//**********************************************************
+NewClass::NewClass(){
+ 
+    isTrue = false;    
+}
+
+//**********************************************************
+//                  Default Constructor
+//**********************************************************
+NewClass::~NewClass(){
+    
+    cout << "\n******Add code to Destroy **player in Yahtzee*****\n";
+    
+}
+//**********************************************************
+//              Setup NewClass here
+//  reset user's hiScore and return true to main()
+//  if true, then Admin will confirm the rewritten record in main()
+//**********************************************************
+
+bool NewClass::start(User &user, const long recordLoc){
+  
+    pause();          
+  
+    int randNum = (rand()%(0+101));             // random number between 0-100
+              
+        
+    isTrue = true; // set flag
+
+    // Reset user's hiSCore and update binary & text file            
+    user.setHiScore( randNum);
+    
+    cout << "\n\tNew High Score of " << user.getHiScore() << "!\n";
+
+    // Certain printout depending on if User is logged in or not
+    if(user.getName() == "Guest"){ 
+
+        //user.printUsr();
+        cout<<"\n\tSign up or Login to save your score.\n"; 
+    }
+    else { 
+        
+        // rewrite this record in binary & text files  
+        user.reWrtBin(recordLoc); 
+        cout << "\nUser is updating binary file...."; 
+    }
+
+    return isTrue; // return true and Admin will confirm files were rewritten
+}
+
+//******************************************
+//      pause screen before continuing
+//******************************************
+void NewClass::pause(char ch) {
+    
+    string msg = ch=='r' ? "roll" : "continue";    
+    cout<<endl<<setw(6)<<' '<<"Press enter to " << msg << ".";   
+    cin.get();
+}
