3c1c27879d513dd7464b0a8728e5af8729e6fb71
koa2019 <no-reply>
2023-04-18 14:40:04 -0700

    deleted build and dist folders. They can mess up the program if its run on a mac versus a pc.
    Signed-off-by: koa2019 <no-reply>

--- store_code_cpp/template_v1.4_userAdminNewClass/build/Debug/Cygwin-Windows/NewClass.o.d
+++
@@ -1,3 +0,0 @@
-build/Debug/Cygwin-Windows/NewClass.o: NewClass.cpp NewClass.h User.h
-NewClass.h:
-User.h:
--- store_code_cpp/template_v1.4_userAdminNewClass/build/Debug/Cygwin-Windows/main.o.d
+++
@@ -1,4 +0,0 @@
-build/Debug/Cygwin-Windows/main.o: main.cpp User.h Admin.h NewClass.h
-User.h:
-Admin.h:
-NewClass.h:
